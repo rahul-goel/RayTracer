286ff352072795d34f287931adbc35b89bfb4840
Rahul Goel <rahulgoellko@gmail.com>
2021-05-17 19:36:56 +0530

    till abstract class for materials

--- main.cpp
+++ main.cpp
@@ -1,14 +1,43 @@
+#include "rtweekend.h"
 #include "color.h"
-#include "vec3.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
+
+//temporary function
+color ray_color(const ray& r, const hittable& world, int depth) {
+	if (depth <= 0) {
+		return color(0, 0, 0);
+	}
+
+	hit_record rec;
+	if (world.hit(r, 0.001, infinity, rec)) {
+		point3 target = rec.p + rec.normal + random_unit_vector();
+		return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+	}
+		vec3 unit_direction = unit_vector(r.direction());
+		auto t = 0.5 * (unit_direction.y() + 1.0);
+		return (1.0 - t) * color(1, 1, 1) + t * color (0.5, 0.7, 1.0);
+}
 
 int main() {
 
 	// Image
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = (int) (image_width / aspect_ratio);
+	const int samples_per_pixel = 100;
+	const int max_depth = 5;
 
-	const int image_width = 256;
-	const int image_height = 256;
+	// world
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+	// camera
+	camera cam;
 
 	// Render
 
@@ -17,8 +46,14 @@ int main() {
 	for (int j = image_height-1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) {
-			color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
-			write_color(std::cout, pixel_color);
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; s++) {
+				auto u = (i + random_double()) / (image_width - 1);
+				auto v = (j + random_double()) / (image_height - 1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world, max_depth);
+			}
+			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 
--- ray.h
+++ ray.h
@@ -11,10 +11,10 @@ class ray {
 		ray() {}
 		ray(const point3 &origin, const vec3& direction) : orig(origin), dir(direction) {}
 
-		point3 origin() { return orig; }
-		vec3 direction() { return dir; }
+		point3 origin() const { return orig; }
+		vec3 direction() const { return dir; }
 
-		point3 at(double t) {
+		point3 at(double t) const {
 			return orig + t * dir;
 		}
 };
